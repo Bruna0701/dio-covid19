a0036657848f319ce5bdb91ea73df848a33ba6d2
Bruna0701 <bruna.souza5153@gmail.com>
2022-06-10 21:11:59 -0300

    criação do projeto DIO-covid19

---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,5 @@
+import Skeleton from '@material-ui/lab/Skeleton'
+
+//import Skeleton from '@material-ui/core/Skeleton'
+
+export default Skeleton
---
+++ src/components/Typography.jsx
@@ -0,0 +1,5 @@
+import Typography from '@material-ui/core/Typography'
+
+//import { Typography } from "../../node_modules/@material-ui/core/index"
+
+export default Typography
